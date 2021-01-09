b35d67c22e96fd2a4fa4546327f8b4a2dccf0f9e
AmiralYogi <carthymariejoseph@gmail.com>
2021-01-09 22:56:37 +0100

    Clean MongoTestListner
    feat

--- src/main/kotlin/mj/carthy/easyutils/listener/MongoTestListener.kt
+++ src/main/kotlin/mj/carthy/easyutils/listener/MongoTestListener.kt
@@ -7,26 +7,21 @@ import org.springframework.core.annotation.AnnotatedElementUtils
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.test.context.TestContext
 import org.springframework.test.context.support.AbstractTestExecutionListener
-import java.util.*
-import java.util.function.Consumer
 import kotlin.reflect.KClass
 
-class MongoTestListener(
-        @Autowired val mongoTemplate: MongoTemplate,
-        private var classesToClear: Array<KClass<*>>?
-): AbstractTestExecutionListener(){
+class MongoTestListener: AbstractTestExecutionListener() {
+
+    @Autowired private var mongoTemplate: MongoTemplate? = null
+    private var classesToClear: Array<KClass<*>>? = null
 
     override fun beforeTestClass(testContext: TestContext) {
         testContext.applicationContext.autowireCapableBeanFactory.autowireBean(this)
-        this.classesToClear = AnnotatedElementUtils.getMergedAnnotation(testContext.testClass, NoSqlConfig::class.java)?.classes
+        this.classesToClear = AnnotatedElementUtils.getMergedAnnotation(testContext.testClass, NoSqlConfig::class.java)?.classes!!
     }
 
     override fun afterTestMethod(testContext: TestContext) {
         val n1ql: NoSql? = AnnotatedElementUtils.getMergedAnnotation(testContext.testMethod, NoSql::class.java)
-        if (n1ql == null) {
-            classesToClear?.forEach { elem ->  mongoTemplate.dropCollection(elem.java) } /*DELETE CLASS DATA*/
-        } else if (n1ql.clearAfter) {
-            classesToClear?.forEach { elem ->  mongoTemplate.dropCollection(elem.java) } /*DELETE CLASS DATA*/
-        }
+        if (n1ql == null) classesToClear!!.forEach { mongoTemplate!!.dropCollection(it.java) } /*DELETE CLASS DATA*/
+        else if (n1ql.clearAfter) classesToClear!!.forEach { mongoTemplate!!.dropCollection(it.java) } /*DELETE CLASS DATA*/
     }
 }
