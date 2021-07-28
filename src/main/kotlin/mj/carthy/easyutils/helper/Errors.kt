497d7c963c9e5168baa76affa5a24c896e685d34
Carthy.Marie-Joseph <carthy.marie-joseph@catalinas-mbp.hq.eapl.europcar.vwfs-ad>
2021-07-28 15:32:11 +0200

    error: add validation_error

--- src/main/kotlin/mj/carthy/easyutils/helper/Errors.kt
+++ src/main/kotlin/mj/carthy/easyutils/helper/Errors.kt
@@ -13,6 +13,7 @@ open class Errors {
         @JvmField val ENTITY_NOT_FOUND: ErrorCode = ErrorCode("ENTITY_NOT_FOUND")
         @JvmField val PROPERTY_NOT_FOUND: ErrorCode = ErrorCode("PROPERTY_NOT_FOUND")
         @JvmField val SERVER_ERROR: ErrorCode = ErrorCode("SERVER_ERROR")
+        @JvmField val VALIDATION_ERROR: ErrorCode = ErrorCode("VALIDATION_ERROR")
         @JvmField val ZODIAC_SIGN_NOT_FOUND: ErrorCode = ErrorCode("ZODIAC_SIGN_NOT_FOUND")
     }
 
